5c38b6049bc7d016034210a86b092bac734c2cc7
hyunguk <sweetycooki@gmail.com>
2025-11-25 10:52:17 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ pension_query     = "INSERT INTO sweetycooki.TB_PENSION (round, winNumbers, bonu
 lucky_last_round  = "SELECT round FROM sweetycooki.TB_LUCKY ORDER BY seq DESC LIMIT 1"
 pension_last_round= "SELECT round FROM sweetycooki.TB_PENSION ORDER BY seq DESC LIMIT 1"
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (HTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
 def db_conn() :
   global conn
   global cursor
   db_host   = 'sweetycooki.mycafe24.com'
+  #db_host   = 'localhost'
   db_user   = 'sweetycooki'
   db_pw     = 'Cafe240110'
   db_dbname = 'sweetycooki'
@@ -26,8 +27,7 @@ def db_conn() :
                            password = db_pw,
                            db       = db_dbname,
                            port     = 3306,
-                           charset  ='utf8'
-                           )
+                           charset  ='utf8')
   except pymysql.err.MySQLError as e :
     print(f"DB 연결 오류 :: {e}")
   cursor = conn.cursor()
@@ -44,32 +44,29 @@ def job_lucky() :
   lucky_soup = BeautifulSoup(lucky_data.text, 'html.parser')
 
   number_list = ""
-  title       = ""
 
   ## 저장되어있는 회차 가져오기
   cursor.execute(lucky_last_round)
   get_data = cursor.fetchone()
+  old_last_round = get_data[0]
 
-  last_round = get_data[0]
-  old_last_round = lucky_soup.select_one('.win_result h4 > strong').text
+  new_round = lucky_soup.select_one('.win_result h4 > strong').text
 
-  if last_round != old_last_round :
+  if new_round != old_last_round :
     for content in lucky_soup.select('.win_result') :
-      title = content.select_one('h4 > strong').text
-
       for num in content.select('span.ball_645') :
         number_list += num.text + ","
 
     if number_list.endswith(','):
       number_list = number_list[:-1]
 
-    lucky_values  = (title, number_list)
+    lucky_values  = (new_round, number_list)
     cursor.execute(lucky_query, lucky_values,)
 
-    print(">> title :: ", title)
+    print(">> title :: ", new_round)
     print(">> luckyNumbers :: ", number_list)
   else :
-    print(">> last_round :: ", last_round)
+    print(">> new_round :: ", new_round)
     print(">> old_last_round :: ", old_last_round)
     print(">> 회차가 같음")
 
@@ -83,22 +80,18 @@ def job_pension() :
 
   win_numbers = ""
   bonus_numbers = ""
-  p_title = ""
 
   ## 저장되어있는 회차 가져오기
   cursor.execute(pension_last_round)
   get_data = cursor.fetchone()
+  old_last_round = get_data[0]
 
-  last_round = get_data[0]
-  old_last_round = pension_soup.select_one('#after720 h4 > strong').text
+  new_round = pension_soup.select_one('#after720 h4 > strong').text
 
-  if last_round != old_last_round :
+  if new_round != old_last_round :
     for content in pension_soup.select('#after720') :
-      p_title = content.select_one('h4 > strong').text
-      group_nums = content.select('.win720_num')
-
       count = 0
-      for nums in group_nums :
+      for nums in content.select('.win720_num') :
         for eachNum in nums.select(".num") :
             if eachNum is not None :
               count += 1
@@ -115,14 +108,14 @@ def job_pension() :
     if bonus_numbers.endswith(','):
       bonus_numbers = bonus_numbers[:-1]
 
-    pension_values  = (p_title, win_numbers, bonus_numbers)
+    pension_values  = (new_round, win_numbers, bonus_numbers)
     cursor.execute(pension_query, pension_values,)
 
-    print(">> title :: ", p_title)
+    print(">> new_round :: ", new_round)
     print(">> winNumbers :: ", win_numbers)
     print(">> bonusNumbers :: ", bonus_numbers)
   else :
-    print(">> last_round :: ", last_round)
+    print(">> new_round :: ", new_round)
     print(">> old_last_round :: ", old_last_round)
     print(">> 회차가 같음")
 
